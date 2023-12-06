5406996b25b8921cf9b163052c26b6482604582a
Adrian <adikosiak2@gmail.com>
2023-12-06 19:30:12 +0100

    connction and endpoints

---
+++ api/routes/questions.mjs
@@ -0,0 +1,14 @@
+import express from "express";
+import database from "../db/conn.mjs";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    let collection = await database.collection("questions");
+    let questions = await collection.find().toArray();
+
+    res.send(questions).status(200);
+})
+
+export default router;
